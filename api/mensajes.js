03e6a50b40733bdf6e0b3403758ba2ab835bc687
Javier P <javier.aleparedes@gmail.com>
2021-07-22 18:24:44 -0300

    Entrega desafio clase 22

---
+++ api/mensajes.js
@@ -0,0 +1,50 @@
+const Mensaje = require('../models/mensajes')
+
+class Mensajes {
+
+    constructor() {
+
+    }
+
+    async devolver() {
+        try {
+            return Mensaje.find({});
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async buscarPorId(id) {
+        try {
+            return Mensaje.findById({ _id: id })
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async guardar(mensaje) {
+        try {
+            return Mensaje.create(mensaje)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async actualizar(id, mensaje) {
+        try {
+            return Mensaje.findByIdAndUpdate(id, mensaje)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async borrar(id) {
+        try {
+            return Mensaje.findByIdAndDelete(id);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+module.exports = new Mensajes();
---
+++ server.js
@@ -0,0 +1,212 @@
+const express = require('express');
+const productos = require('./api/productos');
+const Mensajes = require('./api/mensajes')
+const handlebars = require('express-handlebars')
+const app = express();
+const http = require('http');
+const server = http.Server(app);
+const io = require('socket.io')(server);
+const Faker = require('./models/faker');
+const faker = require('./models/faker');
+
+//CONECTAR CON MONGOOSE A LA DB DE MONGO
+require('./database/connection');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ARCHIVOS ESTÁTICOS
+app.use(express.static('public'));
+
+//CONFIGURAR HANDLEBARS
+app.engine('hbs', handlebars({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: __dirname + '/views/layouts'
+}));
+
+// ESTABLECER MOTOR DE PLANTILLAS
+app.set("view engine", "hbs");
+// DIRECTORIO ARCHIVOS PLANTILLAS
+app.set("views", "./views");
+
+// CREAR ROUTER
+const routerProductos = express.Router()
+const routerMensajes = express.Router()
+
+// USAR ROUTERS
+app.use('/api/productos', routerProductos)
+app.use('/api/mensajes', routerMensajes)
+
+// LISTAR TODOS LOS MENSAJES
+routerMensajes.get('/leer', async (req, res) => {
+    try {
+        let result = await Mensajes.devolver();
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+});
+
+// LISTAR MENSAJES POR ID
+routerMensajes.get('/leer/:id', async (req, res) => {
+    try {
+        let result = await Mensajes.buscarPorId(req.params.id);
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+});
+
+// GUARDAR MENSAJES EN DB
+routerMensajes.post('/guardar', async (req, res) => {
+    try {
+        let result = await Mensajes.guardar(req.body);
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+});
+
+// ACTUALIZAR UN MENSAJE
+routerMensajes.put('/actualizar/:id', async (req, res) => {
+    try {
+        let result = await Mensajes.actualizar(req.params.id, req.body);
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+});
+
+// BORRAR UN MENSAJE
+routerMensajes.delete('/borrar/:id', async (req, res) => {
+    try {
+        let result = await Mensajes.borrar(req.params.id);
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+});
+
+// VISTA-TEST ** FAKER **
+routerProductos.get('/vista-test/', (req, res) => {
+    res.render('vista', { hayProductos: true, productos: Faker.generarProductos(10) })
+})
+
+routerProductos.get('/vista-test/:cant', (req, res) => {
+    let cantidad = req.params.cant    
+    res.render('vista', { hayProductos: true, productos: Faker.generarProductos(cantidad) })
+})
+
+// LISTAR PRODUCTOS
+routerProductos.get('/listar', async (req, res) => {
+    try {
+        let result = await productos.listar();
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+
+    // if (productos.listar().length > 0) {
+    //     res.render('vista', { hayProductos: true, productos: productos.listar() })
+    // } else if (productos.listar().length == 0) {
+    //     res.render('vista', { hayProductos: false })
+    // }
+})
+
+// LISTAR PRODUCTOS POR ID
+routerProductos.get('/listar/:id', async (req, res) => {
+
+    try {
+        let mensajeLista = await productos.listarPorId(req.params.id);
+        res.json(mensajeLista)
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+})
+
+
+// GUARDAR PRODUCTO
+routerProductos.post('/guardar', async (req, res) => {
+    try {
+        let nuevoProducto = {};
+        nuevoProducto.title = req.body.title;
+        nuevoProducto.price = req.body.price;
+        nuevoProducto.thumbnail = req.body.thumbnail;
+        await productos.guardar(nuevoProducto)
+        res.json(nuevoProducto)
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+    // if (productos.listar().length > 0) {
+    //     res.render('vista', { hayProductos: true, productos: productos.listar() })
+    // } else if (productos.listar().length == 0) {
+    //     res.render('vista', { hayProductos: false })
+    // }
+})
+
+//ACTUALIZAR PRODUCTO POR ID
+routerProductos.put('/actualizar/:id', async (req, res) => {
+    try {
+        let nuevoProducto = await productos.actualizar(req.params.id, req.body);
+        res.json(nuevoProducto);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+})
+
+// BORRAR PRODUCTO POR ID
+routerProductos.delete('/borrar/:id', async (req, res) => {
+    let productoBorrado = await productos.borrar(req.params.id);
+    return res.json(productoBorrado);
+})
+
+// DATOS CHAT
+
+const messages = [
+    { autor: 'Juan', texto: '¡Hola! ¿Que tal?' },
+    { autor: 'Pedro', texto: '¡Muy bien! ¿Y vos?' },
+    { autor: 'Ana', texto: '¡Genial!' }
+];
+
+// SE EJECUTA AL REALIZAR LA PRIMERA CONEXION
+io.on('connection', async socket => {
+    console.log('Usuario conectado')
+
+    // GUARDAR PRODUCTO
+    socket.on('nuevo-producto', nuevoProducto => {
+        console.log(nuevoProducto)
+        productos.guardar(nuevoProducto)
+    })
+    // VERIFICAR QUE SE AGREGA UN PRODUCTO
+    socket.emit('guardar-productos', () => {
+        socket.on('notificacion', data => {
+            console.log(data)
+        })
+    })
+    // ACTUALIZAR TABLA
+    socket.emit('actualizar-tabla', productos.listar())
+
+    // GUARDAR Y MANDAR MENSAJES QUE LLEGUEN DEL CLIENTE
+    socket.on("new-message", function (data) {
+        Mensajes.guardar(data)
+        messages.push(data);
+        console.log(data)
+        io.sockets.emit("messages", Mensajes.devolver());
+    });
+
+});
+
+// pongo a escuchar el servidor en el puerto indicado
+const puerto = 8080;
+
+// USO server PARA EL LISTEN
+const svr = server.listen(puerto, () => {
+    console.log(`servidor escuchando en http://localhost:${puerto}`);
+});
+
+
+// en caso de error, avisar
+server.on('error', error => {
+    console.log('error en el servidor:', error);
+});
